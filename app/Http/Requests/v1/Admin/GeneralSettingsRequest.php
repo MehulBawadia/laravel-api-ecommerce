6faf2ff1f8993fae9d9b905cc0f00e4e9008c44c
Mehul Bawadia <mehulbawadia@gmail.com>
2023-04-25 22:02:59 +0530

    Feat: Admin general settings (#10)
    * Integrated API for Admin -> General Settings with passing tests
    * Code formatting via Laravel Pint

---
+++ app/Http/Requests/v1/Admin/GeneralSettingsRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\v1\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GeneralSettingsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth('sanctum')->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'email' => 'required|email:filter',
+        ];
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,9 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Laravel\Sanctum\Sanctum;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -23,5 +25,36 @@ abstract class TestCase extends BaseTestCase
         ], $headers);
 
         return $this->postJson($url, $data, $allHeaders);
+    }
+
+    /**
+     * Create a user for testing.
+     *
+     * @param  array  $data
+     * @return \App\Models\User
+     */
+    public function createUser($data = [])
+    {
+        $payload = array_merge([
+            'email' => 'admin@example.com',
+            'password' => bcrypt('Password'),
+        ], $data);
+
+        return User::factory()->create($payload);
+    }
+
+    /**
+     * Sign in the administrator.
+     *
+     * @param  array  $data
+     * @return \App\Models\User
+     */
+    public function signInAdmin($data = [])
+    {
+        $user = $this->createUser($data);
+
+        Sanctum::actingAs($user, ['*']);
+
+        return $user;
     }
 }
