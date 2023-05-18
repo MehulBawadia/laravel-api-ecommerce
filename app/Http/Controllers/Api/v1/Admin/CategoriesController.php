d13ec66a27bfc1c7e386a9c07a65c971e575ab88
Mehul <mehulbawadia@gmail.com>
2023-05-18 22:10:56 +0530

    Added documentation for categories

--- app/Http/Controllers/Api/v1/Admin/CategoriesController.php
+++ app/Http/Controllers/Api/v1/Admin/CategoriesController.php
@@ -8,24 +8,45 @@ use App\Http\Requests\v1\Admin\Categories\UpdateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 
+/**
+ * @group Administrator Endpoints
+ *
+ * @subgroup Categories
+ */
 class CategoriesController extends Controller
 {
     /**
-     * Get and paginate the categories.
+     * List All categories
      *
-     * @return void
+     * Display all the categories with pagination.
+     * At a time, there are total of 16 records that will be displayed.
+     *
+     * @queryParam page integer The page number. Defaults to 1. Example: 1
+     *
+     * @responseFile storage/responses/admin/categories/list-all.json
+     *
+     * @authenticated
+     *
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
         $categories = Category::select([
             'id', 'name', 'slug', 'meta_title', 'meta_description', 'meta_keywords',
-        ])->paginate(16);
+        ])->paginate(16)->withQueryString();
 
         return $this->successResponse('', $categories);
     }
 
     /**
-     * Store a new category.
+     * Add new category
+     *
+     * Create a new category and store it's details.
+     *
+     * @responseFile status=201 storage/responses/admin/categories/created.json
+     * @responseFile status=422 storage/responses/admin/categories/validation-errors.json
+     *
+     * @authenticated
      *
      * @return \Illuminate\Http\JsonResponse
      */
@@ -50,7 +71,16 @@ class CategoriesController extends Controller
     }
 
     /**
+     * Get single category
+     *
      * Fetch the details about the given category id.
+     *
+     * @urlParam id integer required The id of the category. Example: 1
+     *
+     * @responseFile status=200 storage/responses/admin/categories/fetch-single.json
+     * @responseFile status=404 storage/responses/admin/categories/not-found.json
+     *
+     * @authenticated
      *
      * @param  int  $id
      * @return \Illuminate\Http\JsonResponse
@@ -68,7 +98,17 @@ class CategoriesController extends Controller
     }
 
     /**
+     * Update category
+     *
      * Update the category details of the given id.
+     *
+     * @urlParam id integer required The id of the category. Example: 1
+     *
+     * @responseFile status=200 storage/responses/admin/categories/updated.json
+     * @responseFile status=404 storage/responses/admin/categories/not-found.json
+     * @responseFile status=422 storage/responses/admin/categories/validation-errors.json
+     *
+     * @authenticated
      *
      * @param  int  $id
      * @return \Illuminate\Http\JsonResponse
@@ -99,7 +139,19 @@ class CategoriesController extends Controller
     }
 
     /**
+     * Delete a category
+     *
      * Delete the category details of the given id.
+     * This will soft delete the category.
+     * Meaning the record will be present in the database, however,
+     * it won't be available to access.
+     *
+     * @urlParam id integer required The id of the category. Example: 1
+     *
+     * @responseFile status=200 storage/responses/admin/categories/deleted.json
+     * @responseFile status=404 storage/responses/admin/categories/not-found.json
+     *
+     * @authenticated
      *
      * @param  int  $id
      * @return \Illuminate\Http\JsonResponse
