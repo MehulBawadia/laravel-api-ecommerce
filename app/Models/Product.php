21daec26ffabbe062a24e4b2af3a51c8536c2732
Mehul <mehulbawadia@gmail.com>
2023-05-23 21:27:17 +0530

    Products API - WIP

---
+++ app/Models/Product.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Product extends Model implements HasMedia
+{
+    use HasFactory, SoftDeletes, InteractsWithMedia;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'slug', 'description', 'quantity', 'rate',
+        'brand_id', 'category_id',
+        'meta_title', 'meta_description', 'meta_keywords',
+    ];
+
+    /**
+     * A product belongs to a single brand.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    /**
+     * A product belongs to a single category.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Register the media collection for generating images.
+     * It will also generate necessary responsive images.
+     */
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('product-images')
+            ->withResponsiveImages();
+    }
+
+    /**
+     * Do some processing with the model events related to Product.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        Product::creating(function ($model) {
+            $model->slug = Str::slug($model->name);
+        });
+        Product::updating(function ($model) {
+            $model->slug = Str::slug($model->name);
+        });
+    }
+}
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Brand;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $words = ucwords(fake()->words(5, true)),
+            'slug' => Str::slug($words),
+            'description' => fake()->sentences(5, true),
+            'quantity' => fake()->randomDigit(),
+            'rate' => fake()->randomFloat(2, 10, 1000),
+            'brand_id' => Brand::factory(),
+            'category_id' => Category::factory(),
+            'meta_title' => "{$words} ".config('app.name'),
+            'meta_description' => "{$words} ".config('app.name'),
+            'meta_keywords' => "{$words} ".config('app.name'),
+        ];
+    }
+}
